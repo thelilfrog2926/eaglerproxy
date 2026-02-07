00b4d603dc4db667fbc82d761aba45b467053ba5
thelilfrog2926 <kramankkhatri90@gmail.com>
2026-02-07 08:56:55 -0600

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ import { Config } from "./launcher_types.js";
 export const config: Config = {
   adapter: {
     name: "EaglerProxy",
-    bindHost: "TheLilFrog.aternos.me",
-    bindPort: 37587,
+    bindHost: "0.0.0.0",
+    bindPort: 10000,
     maxConcurrentClients: 20,
     // set this to false if you are unable to install sharp due to either the use of a platform that does not support native modules
     // or if you are unable to install the required dependencies. this will cause the proxy to use jimp instead of sharp, which may
